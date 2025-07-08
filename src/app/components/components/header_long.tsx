a02ecc826e68c5de156d7e01a4d6eba623630746
Kaytiue7 <Kaytiue7@users.noreply.github.com>
2025-07-08 19:59:13 +0300

    puısh

--- src/app/components/components/header_long.tsx
+++ src/app/components/components/header_long.tsx
@@ -1,19 +1,30 @@
+import { FiArrowRight } from "react-icons/fi";
+import { useState } from "react";
+import PricingTable from "./priceModal";
+
 export default function Header_Short() {
    
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div 
       style={{ backgroundImage: `url('/main.jpg')` }}
       className="relative w-full h-screen object-cover bg-cover bg-center flex items-center"
     >
       <div className="flex flex-col justify-center items-center text-center text-white px-2 w-full h-full md:bg-transparent md:w-auto md:h-auto">
-        <h2 className="text-3xl font-extrabold leading-tight mb-4 md:text-4xl md:font-semibold md:px-[20%] md:mb-0" style={{textShadow: '0 2px 8px rgba(0,0,0,0.7)'}}>
+        <h2 className="text-3xl font-extrabold leading-tight mb-4 md:text-6xl md:font-semibold md:px-[10%] px-[2%] md:mb-10" style={{textShadow: '0 2px 8px rgba(0,0,0,0.7)'}}>
           It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
         </h2>
-        <button  
-          className="px-6 py-2 bg-[#6E71FC] text-white rounded-md hover:bg-[#6366ed] transition text-base font-bold shadow-md shadow-black flex items-center justify-center md:text-xl md:mt-4 md:w-auto md:max-w-none"
+        <button
+          className="px-10 py-2 bg-blue-700 cursor-pointer text-white rounded-md hover:bg-blue-800 transition text-2xl shadow-md shadow-black flex items-center justify-center  md:mt-4 md:w-auto md:max-w-none"
+          onClick={() => setShowModal(true)}
         >
-          Teklif Al! <span className='ml-2'>→</span>
+          Teklif Al
+          <span className="ml-2 flex items-center text-md">
+            <FiArrowRight />
+          </span>
         </button>
+        {showModal && <PricingTable />}
       </div>
     </div>
   );
