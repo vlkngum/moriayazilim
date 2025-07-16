05c95d662b0ad70b6b439dcf5bfe867ab08c8453
Kaytiue7 <Kaytiue7@users.noreply.github.com>
2025-07-17 00:43:17 +0300

    commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://moriayazilim.com', // kendi domainini yaz
+    generateRobotsTxt: true,           // robots.txt dosyası da oluşsun
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
