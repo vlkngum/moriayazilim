a1ea44c92033bee1ca502269988d04b9e04d73ec
Efe Mert CABAOĞLU <efe@moriayazilim.com>
2025-07-29 23:43:27 +0300

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -13,36 +13,15 @@ module.exports = {
       const posts = res.data;
       console.log('Blog post sayısı:', posts.length);
 
-      return posts.map(post => `/blog/${post.id}`);
+      return posts.map(post => ({
+        loc: `/blog/${post.id}`,
+        changefreq: 'weekly',
+        priority: 0.8,
+        lastmod: new Date(post.updatedAt).toISOString(),
+      }));
     } catch (error) {
       console.error('Sitemap için blog verisi alınamadı:', error);
       return [];
     }
-  },
-
-  transform: async (config, path) => {
-    if (path.startsWith('/blog/')) {
-      try {
-        const res = await axios.get('https://moriayazilim.com/api/get-blog');
-        const posts = res.data;
-        const post = posts.find(p => `/blog/${p.id}` === path);
-        if (post) {
-          return {
-            loc: path,
-            changefreq: 'weekly',
-            priority: 0.8,
-            lastmod: new Date(post.updatedAt).toISOString(),
-          };
-        }
-      } catch (error) {
-        console.error('Transform için blog verisi alınamadı:', error);
-      }
-    }
-
-    return {
-      loc: path,
-      changefreq: 'weekly',
-      priority: 0.7,
-    };
   },
 };
